6a696e8941731c3f887eccb0aaa30257a5b4f120
Zayn Khan <zaynkhan100110@gmail.com>
2026-07-22 22:07:04 -0700

    answer of Qna 1
    try your self first to slove this question is from exercise 3 to 4.3

---
+++ Qn1_ans.py
@@ -0,0 +1,8 @@
+
+
+# This file contain answer of Qna Quc 1
+
+
+variable = ('Laptop')  # just string with value assign to the variable
+variable = (variable[3:] + ' one')  # using the last varible i silce first 3 letters from front side then add 5 letters with a space (makes 2 words) then assign the result to the new variable 
+print ("'"+variable.capitalize()+'"')  # print the new variable with capitalize (capitalize makes first letter of a sentence big case and rest of in small case
